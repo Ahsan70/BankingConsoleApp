56d7a8cf234f3d82c4dd1db07d19289790081be4
Ahsan Ali <ahsan-ali70@outlook.com>
2021-08-18 02:00:27 +0500

    Created Login Form

--- BankingUI/Program.cs
+++ BankingUI/Program.cs
@@ -10,7 +10,28 @@ namespace BankingUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("********HBL**********");
+            Console.WriteLine(":: Login Page ::");
             
+
+            string userName = null, password = null;
+            Console.Write("Username");
+            userName = Console.ReadLine();
+
+            if (userName != "")
+            {
+                Console.Write("Password");
+                password = Console.ReadLine();
+            }
+
+            if (userName == "Ahsan" && password == "123")
+            {
+                Console.WriteLine("TO Do : Menu Here");
+
+            }
+
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
         }
     }
 }
